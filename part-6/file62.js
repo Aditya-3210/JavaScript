6d3400ee6b7aa0092a61f2c3d9f19cae029ccd18
Aditya-3210 <207103591+Aditya-3210@users.noreply.github.com>
2026-02-18 16:25:06 +0530

    Updated file62.js

--- part-6/file62.js
+++ part-6/file62.js
@@ -1,2 +1,13 @@
-let num = 5;
-console.log(first)
+// let num = 5;
+// console.log(first)
+
+let num=0;
+console.log(num || 10)
+//here num is false
+//returns true value => 10 
+
+
+console.log(num ?? 10)
+//nullish coalescing operator
+//checks for null or undefined
+//since num=0(not null) => 0
